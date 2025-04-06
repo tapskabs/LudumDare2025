ce4a237491d332bc758b395ecedd6b2aca6bd8a6
Lilycorpzz <143013925+Lilycorpzz@users.noreply.github.com>
2025-04-06 17:11:56 +0200

    Update EnemyStats.cs

--- Assets/Scripts/EnemyStats.cs
+++ Assets/Scripts/EnemyStats.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class EnemyStats : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float maxHealth = 30f;
+    [SerializeField] private float currentHealth;
+
+    [Header("Visual Feedback")]
+    [SerializeField] private GameObject hitEffectPrefab;
+    [SerializeField] private AudioClip hitSound;
+
+    private Animator animator;
+
+    private void Start()
     {
-        
+        currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(float damage)
     {
-        
+        currentHealth -= damage;
+
+        // Visual/Audio feedback
+        if (hitEffectPrefab) Instantiate(hitEffectPrefab, transform.position, Quaternion.identity);
+        if (hitSound) AudioSource.PlayClipAtPoint(hitSound, transform.position);
+
+        animator?.SetTrigger("Hurt");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        animator?.SetTrigger("Death");
+        GetComponent<Collider>().enabled = false;
+        Destroy(gameObject, 2f); // Delay to allow death animation
     }
 }
